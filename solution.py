3b03f5942b86998f6dc12f9f2c66a7b9b4914e6b
Aleksandra Kuleva <129543447+Seeingin@users.noreply.github.com>
2023-04-11 14:08:00 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,8 +7,8 @@ chat_id = 752592494
 def solution(x: np.array, y: np.array) -> bool:
     import scipy.stats as stats
 
-    a = scipy.stats.ks_2samp(x, y, alternative='two-sided', method='auto')
-    pvalue = a[1]
+    res = scipy.stats.ks_2samp(x, y, alternative='two-sided', method='auto')
+    pvalue = res.pvalue
     if pvalue > 0.07:
       return False
     else:
