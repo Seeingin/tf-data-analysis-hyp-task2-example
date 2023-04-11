eadb30b93c7bf0e2c77b7cef79031dfa4922d58e
Aleksandra Kuleva <129543447+Seeingin@users.noreply.github.com>
2023-04-11 19:22:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,8 +7,10 @@ chat_id = 752592494
 def solution(x: np.array, y: np.array) -> bool:
     import scipy
     import scipy.stats as stats
-
-    res = stats.cramervonmises_2samp(x, y)
+    import warnings
+    warnings.filterwarnings("ignore")
+    
+    res = stats.anderson_ksamp([x,y])
     pvalue = res.pvalue
     if pvalue > 0.07:
       return False
